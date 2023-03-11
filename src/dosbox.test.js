e02b44208aac1593cdd5b73aac8b09ca691f813d
joncloud <jdberube@gmail.com>
2023-03-10 21:20:46 -0800

    More faster tests

--- src/dosbox.test.js
+++ src/dosbox.test.js
@@ -1,4 +1,4 @@
-import { mkdtemp, readFile, rm } from 'fs/promises';
+import { mkdtemp, readFile, rm, writeFile } from 'fs/promises';
 import { invokeDosbox } from './dosbox.js';
 import { describe, before, after, it } from 'mocha';
 import { join } from 'path';
@@ -8,10 +8,19 @@ import { PassThrough } from 'stream';
 describe('dosbox', function () {
   /** @type {string} */
   let tempPath;
+  /** @type {string} */
+  let configFile;
   before(async function () {
     tempPath = await mkdtemp('dos-build-action', {
       encoding: 'utf8',
     });
+    configFile = join(tempPath, 'dosbox.conf');
+    await writeFile(configFile, `[dosbox]
+memsize=256
+
+[cpu]
+cycles=max
+turbo=true`);
   });
 
   after(async function () {
@@ -31,6 +40,7 @@ describe('dosbox', function () {
         'C:',
         'ECHO FOO BAR > TEMP.TXT',
       ],
+      configFile,
       stderr: new PassThrough(),
       stdout: new PassThrough(),
     });
